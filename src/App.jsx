8f84aa2a6b5297f3dc1f04e18a801fbdaf41b6c9
Erick Tolentino <ericktolentino@pursuit.org>
2024-01-25 17:52:41 -0500

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 import Home from "./pages/Home"
+import SingleMeeting from "./pages/singleMeeting";
+import SingleBooking from "./pages/singleBooking";
+import Bookings from "./pages/Bookings";
 
 function App() {
   return (
@@ -8,6 +11,9 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/bookings/:event_id" element={<SingleBooking />} />
+          <Route path="/bookings/new" element={<Bookings/>}/>
+
         </Routes>
       </Router>
     </div>
