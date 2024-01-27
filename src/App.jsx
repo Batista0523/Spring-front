9677bc2d5aa7340cffb3a6629678f6f43b220e9b
elisaul batista <elisaulbatista@pursuit.org>
2024-01-27 16:14:26 -0500

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,26 @@
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import Home from "./pages/Home"
+import Home from "./pages/Home";
 import SingleMeeting from "./pages/singleMeeting";
 import Index from "./components/Index";
 import Bookings from "./pages/Bookings";
 import NewRoom from "./components/NewRoom";
-import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div>
       <Router>
-      <div className="nav">
-          <NavBar />
-        </div>
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/events" element={<Index />} />
-          <Route path="/new" element={<NewRoom/>} />
+          <Route path="/new" element={<NewRoom />} />
           {/* <Route path="/bookings/:event_id" element={<SingleBooking />} /> */}
-          <Route path="/bookings/new" element={<Bookings/>}/>;
-
+          <Route path="/bookings/new" element={<Bookings />} />;
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,47 +1,22 @@
-
-import { Link } from "react-router-dom"
-
-export default function NavBar() {
-    return (
-        // <div className="navbar" >
-        //     <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
-        //         <div className="container-fluid">
-        
-                    <div className="menu">
-                        <Link to="/events">Meeting Rooms </Link>
-
-                        <Link to="/bookings">Bookings</Link>
-
-                        <Link to="/new">New Room </Link>
-                        
-                    </div>
-        //         </div>
-        //     </nav>
-        // </div>
-    )
+import React from "react";
+import { Link } from "react-router-dom";
+import './navbar.css'
+function NavBar() {
+  return (
+    <div>
+      <div className="navBar-menu">
+        <Link to="/">
+          <p className="nav-menu">Meeting Rooms</p>
+        </Link>
+        <Link to="/bookings/new">
+          <p className="nav-menu">Bookings</p>
+        </Link>
+        <Link to="/new">
+          <p className="nav-menu">New Room</p>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import './navbar.css'
-// function NavBar() {
-//   return (
-//     <div>
-//       <div className="menu">
-//         <Link to="/">
-//           <p className="nav-menu">Meeting Rooms</p>
-//         </Link>
-//         <Link to="/bookings/new">
-//           <p className="nav-menu">Bookings</p>
-//         </Link>
-//         <Link to="/new">
-//           <p className="nav-menu">New Room</p>
-//         </Link>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default NavBar;
-
+export default NavBar;
