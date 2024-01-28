5c010858ad60c484c0c0396723d37bf57e9a43dc
arim83 <jorammercado@pursuit.org>
2024-01-27 23:16:20 -0500

    css event space elements

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { getAllItems } from "../helpers/helperFunc";
+import { Link } from "react-router-dom";
 import Event from "../components/Event";
 import "./index.css";
 
@@ -24,31 +25,36 @@ const Index = ({ startdate }) => {
     <div className="container-fluid">
       <div className="find-menu">
         <h3>Finds Available Rooms:</h3>
-        <label className="input menu" htmlFor="">
-            Start:
-            <input type="text"  />
-            End:
-            <input type="text" />
-            Floor: 
-            <input type="number" />
-            Capacity:
-            <input type="number" />
-            <button className="btn-find">Find</button>
+        <label className="input menu" >
+          Start:
+          <input type="text" />
+          End:
+          <input type="text" />
+          Floor:
+          <input type="number" />
+          Capacity:
+          <input type="number" />
+          <button className="btn-find">Find</button>
         </label>
       </div>
-      <div className="row">
-        {items.map((item) => (
-          <div key={item.event_id} className="col-lg-4 col-md-6 mb-4">
-           
-            <Event
-           
-              eventId={item.event_id}
-              eventname={item.eventname}
-              capacity={item.capacity}
-              floor={item.floor}
-            />
-          </div>
-        ))}
+      <hr className="line"></hr>
+      <div className="bookings">
+        <ul>
+          {items.map((item) => (
+            <Link to={`/events/${item.event_id}`} >
+              <li key={item.event_id} >
+
+                <Event
+
+                  eventId={item.event_id}
+                  eventname={item.eventname}
+                  capacity={item.capacity}
+                  floor={item.floor}
+                />
+              </li>
+            </Link>
+          ))}
+        </ul>
       </div>
     </div>
   );
